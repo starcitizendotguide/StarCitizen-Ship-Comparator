d0da5391a1fe943b4c70d765617049979a015214
sweetcode <jan@krueger-jan.de>
2016-09-19 17:48:35 +0200

    removed imageDownload script

--- imageDownload/download.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-$rawData = json_decode(file_get_contents(__DIR__ . '/../parser/raw.json'), true);
-
-foreach($rawData as $value) {
-    file_put_contents(__DIR__ . '/../assets/images/' . $value['name'] . '.jpg', file_get_contents('https://robertsspaceindustries.com' . $value['media'][0]['images']['avatar']));
-}
