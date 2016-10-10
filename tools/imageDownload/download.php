dda7ceecb11419823ab84a9e42994f598d96b247
vagrant <vagrant@homestead>
2016-10-10 10:02:12 +0000

    one new ship <3

--- tools/imageDownload/download.php
+++ tools/imageDownload/download.php
@@ -1,8 +1,8 @@
 <?php
 
 
-$rawData = json_decode(file_get_contents(__DIR__ . '/raw.json'), true);
+$rawData = json_decode(file_get_contents(__DIR__ . '/../parserImproved/raw.json'), true);
 
 foreach($rawData as $value) {
-    file_put_contents(__DIR__ . '/../assets/images/' . $value['name'] . '.jpg', file_get_contents('https://robertsspaceindustries.com' . $value['media'][0]['images']['avatar']));
+    file_put_contents(__DIR__ . '/images/' . $value['name'] . '.jpg', file_get_contents('https://robertsspaceindustries.com' . $value['media'][0]['images']['avatar']));
 }
