b5673d5b47a6be900fb8efb4bdb076a9b19573dd
sweetcode <jan@krueger-jan.de>
2016-11-18 22:49:05 +0100

    new ships :)

--- tools/imageDownload/download.php
+++ tools/imageDownload/download.php
@@ -3,6 +3,7 @@
 
 $rawData = json_decode(file_get_contents(__DIR__ . '/../parserImproved/raw.json'), true);
 
+echo 'Processing the ' . $value['name'] . PHP_EOL;
 foreach($rawData as $value) {
     file_put_contents(__DIR__ . '/images/' . $value['name'] . '.jpg', file_get_contents('https://robertsspaceindustries.com' . $value['media'][0]['images']['avatar']));
 }
